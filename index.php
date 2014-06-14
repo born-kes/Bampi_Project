cb10c781c88b07c4b9b472b97acc8f0acb3d18f4
born-kes <born-kes@o2.pl>
2014-06-14 16:30:40 +0200

    Ładowanie skórki i danych

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-<?php $start = microtime(true);
+<?php $microTimeStart = microtime(true);//  header("Location: ./admin/");
     ob_start();	session_start();	error_reporting(E_ALL ^ E_NOTICE);
 
 $typ=(isset($_GET['typ'])?$_GET['typ']:'html');
 require_once('inc/functions.php');
+print_r($_GET);
+$Temple = temple(@$_GET['page']);
 
-require_once('inc/themes.php');
+$Temple['blok'] = array_conect($Temple['blok'], $config);
 
-$Temple = file_get_contents('themes/'.$config['theme'].'/template.html');
-echo swap_string($Temple,$BLOKS);
-
+echo stringSwap($Temple['theme'], $Temple['blok']);
 
     ob_end_flush();
-?>
