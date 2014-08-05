5f0d387d4f3b3df75942494835b177025d11c15a
born-kes <born-kes@o2.pl>
2014-08-05 13:22:49 +0200

    Masowe zasysanie z Ceneo

---
+++ module/zasysanie/zasysanie.php
@@ -0,0 +1,12 @@
+<?php  //  $this->file('edyt.js')->data();
+$_table['html'] = $this->file('zasysacz.html')->data();
+$_table['js'] = '';
+$_table['css'] = '';
+
+return array(
+    'content'=>$_table,
+    'title'=> '',
+    'js'=> '',
+    'css'=> '',
+    'head'=>''
+);
