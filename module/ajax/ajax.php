ea28f504a581320616700b5907d5c100b933e893
born-kes <born-kes@o2.pl>
2014-08-05 13:10:32 +0200

    Ajax dla sql

--- module/ajax/ajax.php
+++ module/ajax/ajax.php
@@ -1,4 +1,15 @@
 <?php
-print_r($_GET);
+
+if( !isset($_POST['id']) ){
     echo microtime(true);
-exit;
+    exit;
+}else{
+    $this->loadInclude("module/sql/sql.php");
+     sql();
+    exit;
+}
+//    switch($_POST['form'])
+//    {
+//        case 'thead':break;
+//    }
+
